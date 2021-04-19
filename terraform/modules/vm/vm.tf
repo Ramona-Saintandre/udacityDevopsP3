cd33aac2c29d69758fddc9f97721b1c02ebeccf5
ramona-Saintandre <codinggrandma@gmail.com>
2021-04-18 20:38:05 -0400

    added name of linux machine to vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,26 +1,26 @@
-resource "azurerm_network_interface" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
+resource "azurerm_network_interface" "vm-test-automation" {
+  name                = "test-nic"
+  location            = var.location
+  resource_group_name = var.resource_group
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = ""
+    name                          = "ipconfig"
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = ""
+    public_ip_address_id          = var.public_ip_address_id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "" {
-  name                = ""
-  location            = ""
-  resource_group_name = ""
-  size                = ""
-  admin_username      = ""
-  network_interface_ids = []
+resource "azurerm_linux_virtual_machine" "test" {
+  name                  = "test-vm"
+  location              = var.location
+  resource_group_name   = var.resource_group
+  size                  = "Standard_B1s"
+  admin_username        = var.admin_username
+  network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
-    username   = ""
-    public_key = "file("~/.ssh/id_rsa.pub")"
+    username   = var.admin_username
+    public_key = file("/home/vsts/work/_temp/id_rsa.pub")
   }
   os_disk {
     caching           = "ReadWrite"
@@ -29,7 +29,7 @@ resource "azurerm_linux_virtual_machine" "" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
