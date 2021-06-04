f151f6a9c3a04e3fb96234c96eb2f3f28938b3c7
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-03 22:14:30 -0400

    removed packer from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,6 +62,5 @@ module "vm" {
   resource_group  = module.resource_group.resource_group_name
   public_ip       = module.publicip.public_ip_address_id
   admin_username  = var.admin_username
-  #packer_image    = var.packer_image
   public_key_path = var.public_key_path
 }
