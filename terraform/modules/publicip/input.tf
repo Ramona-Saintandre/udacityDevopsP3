130ae8956cf13e5448a04cb6c1b4572c2c1d7ff1
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-06 00:41:50 -0400

    update security input.tf

--- terraform/modules/publicip/input.tf
+++ terraform/modules/publicip/input.tf
@@ -1,6 +1,6 @@
-# Resource Group/Location
 variable "location" {}
-variable "resource_group" {}
-
 variable "application_type" {}
 variable "resource_type" {}
+variable "resource_group" {}
+variable "subnet_id" {}
+variable "address_prefix_test" {}
