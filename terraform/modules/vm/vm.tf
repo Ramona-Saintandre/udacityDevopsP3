9b11662a2f1f37658588138d1a65c761eaeff8f8
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-06 18:24:47 -0400

    fixed line 20 in vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_interface" "vm-test" {
+resource "azurerm_network_interface" "test" {
   name                = "test-nic"
   location            = var.location
   resource_group_name = var.resource_group
@@ -17,11 +17,11 @@ resource "azurerm_linux_virtual_machine" "test" {
   resource_group_name   = var.resource_group
   size                  = "Standard_B1s"
   admin_username        = var.admin_username
-  network_interface_ids = azurerm_network_interface.test.id
+  network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("/home/vsts/work/_temp/id_rsa.pub")
-    //public_key = file("c:/users/thene/.ssh/id_rsa.pub")
+    //public_key = file("/home/vsts/work/_temp/id_rsa.pub")
+    public_key = file("c:/users/thene/.ssh/id_rsa.pub")
   }
   os_disk {
     caching           = "ReadWrite"
