7cab8d837ee08f97ef827a9cfd2867359cd60cde
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-06 00:36:31 -0400

    updated appservice.input.tf

--- terraform/modules/appservice/input.tf
+++ terraform/modules/appservice/input.tf
@@ -1,8 +1,6 @@
+# Resource Group/Location
 variable "location" {}
-variable "resource_group" {}
-# Resource Virtual Machine
+
 variable "application_type" {}
 variable "resource_type" {}
-variable "public_ip_address_id" {}
-variable "subnet_id" {}
-variable "admin_username" {}
+variable "resource_group" {}
