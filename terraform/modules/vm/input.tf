81f20ae86d27c957bf11b680d7fafd6561762b2d
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-06 00:07:32 -0400

    updated input.tf for VM

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,1 +1,8 @@
-
+variable "public_ip" {}
+variable "resource_group" {}
+variable "location" {}
+variable "name" {}
+variable "subnet_id" {}
+variable "admin_username" {}
+variable "packer_image" {}
+variable "public_key_path" {}
