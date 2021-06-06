46c219c1ccfe267e3c2731cad883c02149f58049
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-05 21:31:02 -0400

    modified tfvars file

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ terraform {
     storage_account_name = "udacityp3storage"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
-    access_key           =  ""
+    access_key           =  "pMebkXhJEroMrgLYthf54aq+gDqfFO5cNP/ECUD1VOGYgK7sEie52DnYgdSY48qUxGLY7rU0nUwJOAhWyWrICw=="
   }
 }
 module "resource_group" {
