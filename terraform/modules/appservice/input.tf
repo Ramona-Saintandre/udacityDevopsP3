80241b63f2802bbf2a81347c55848621ad64c134
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-06 00:21:22 -0400

    updated VM input file

--- terraform/modules/appservice/input.tf
+++ terraform/modules/appservice/input.tf
@@ -1,6 +1,8 @@
-# Resource Group/Location
 variable "location" {}
-variable "public_ip_address_id" {}
+variable "resource_group" {}
+# Resource Virtual Machine
 variable "application_type" {}
 variable "resource_type" {}
-variable "resource_group" {}
+variable "public_ip_address_id" {}
+variable "subnet_id" {}
+variable "admin_username" {}
