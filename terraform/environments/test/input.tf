121364f9ac134026e6d6fde452bfcb7915e72c3e
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-06 21:03:47 -0400

    created environments-test folder

---
+++ terraform/environments/test/input.tf
@@ -0,0 +1,22 @@
+# Azure GUIDS
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+
+# Resource Group/Location
+variable "location" {}
+variable "resource_group" {}
+variable "webapp_name" {}
+
+# Network
+variable virtual_network_name {}
+variable "address_prefix_test" {}
+variable address_space {}
+
+# Virtual Machine
+variable vm_admin_username {}
+variable vm_public_key {}
+
+# Tags
+variable "project" {}
