b544f874c9715c3f7f122804e4b5dd079268bea8
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-14 02:00:13 -0400

    updated script

--- Storagescript.sh
+++ Storagescript.sh
@@ -9,8 +9,9 @@ az group create --name $RESOURCE_GROUP_NAME --location eastus
 # Create storage account
 az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
 
-# Get storage account key
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
+# Get storage account key
+ACCOUNT_KEY=$(az storage account keys list --resource-group udacityP3-RG --account-name udacityp3storageacct --query '[0].value' -o tsv)
 
 # Create blob container
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
