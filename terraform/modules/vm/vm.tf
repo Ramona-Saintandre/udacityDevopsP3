45cd13d3667daec5ab9aa044f7c123eb8bdceef2
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-16 06:33:52 -0400

    updated vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -11,7 +11,7 @@ resource "azurerm_network_interface" "test" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "test" {
+resource "azurerm_linux_virtual_machine" "Udacityp3_test" {
   name                  = "udacityp3-test-vm"
   location              = var.location
   resource_group_name   = var.resource_group
