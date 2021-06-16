9467a8ccdc16c7a3f8f058b4e8f596f45458821c
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-15 21:04:34 -0400

    removed resource type, updated YAML

--- terraform/input.tf
+++ terraform/input.tf
@@ -8,7 +8,7 @@ variable "tenant_id" {}
 variable "location" {}
 variable "resource_group" {}
 variable "application_type" {}
-variable "resource_type" {}
+
 
 # Network
 variable virtual_network_name {}
--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -1,11 +1,11 @@
 resource "azurerm_virtual_network" "test" {
-  name                 = "P3-testvnet"
+  name                 = "UdacityP3-testvnet"
   address_space        = var.address_space
   location             = var.location
   resource_group_name  = var.resource_group
 }
 resource "azurerm_subnet" "test" {
-  name                 = "P3-testsubnet"
+  name                 = "UdacityP3-testsubnet"
   resource_group_name  = var.resource_group
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefix       = var.address_prefix_test
