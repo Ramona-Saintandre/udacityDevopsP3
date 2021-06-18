77c9e6e61a077488e5ac5c13a07a400541dd7675
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-17 20:45:44 -0400

    commented out line 61

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,7 @@ module "vm" {
   source          = "./modules/vm"
   location        = var.location
   application_type = var.application_type
-  resource_type    = "VM"
+  #resource_type    = "VM"
   subnet_id       = module.network.subnet_id_test
   resource_group  = module.resource_group.resource_group_name
   public_ip_address_id = module.publicip.public_ip_address_id
