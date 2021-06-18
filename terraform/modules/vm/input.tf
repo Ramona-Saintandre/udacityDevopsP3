4c6767ec9c9b885d6974228ee07a4fb1ff1b1f2f
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-17 22:39:36 -0400

    updated vm input.tf

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -3,7 +3,7 @@ variable "location" {}
 variable "resource_group" {}
 # Resource Virtual Machine
 variable "application_type" {}
-
+variable "resource_type" {}
 variable "public_ip_address_id" {}
 variable "subnet_id" {}
 variable "admin_username" {}
