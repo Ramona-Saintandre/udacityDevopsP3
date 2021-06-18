3df0b7f972c50f4adef58a5a2122b42563553a94
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-18 16:42:29 -0400

    updated nsg input.tf file

--- terraform/modules/networksecuritygroup/input.tf
+++ terraform/modules/networksecuritygroup/input.tf
@@ -4,4 +4,4 @@ variable "application_type" {}
 variable "resource_type" {}
 variable "resource_group" {}
 variable "subnet_id" {}
-variable "address_prefixes_test" {}
+variable "address_prefix_test" {}
