3f33f80434cacc24cfe2a067837d88d87fba9d2b
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-17 20:40:25 -0400

    updated main/vm.tf

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -20,8 +20,9 @@ resource "azurerm_linux_virtual_machine" "test" {
   network_interface_ids = [azurerm_network_interface.test.id]
   admin_ssh_key {
     username   = var.admin_username
+     public_key = file("c:/users/thene/.ssh/id_rsa.pub")
     //public_key = file("/home/vsts/work/_temp/id_rsa.pub")
-    //public_key = file("c:/users/thene/.ssh/id_rsa.pub")
+   
   }
   os_disk {
     caching           = "ReadWrite"
