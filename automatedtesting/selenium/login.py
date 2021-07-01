82922fb7bbb81f041fb5dcf9644584dd1429eac7
ramona-Saintandre <codinggrandma@gmail.com>
2021-06-30 20:43:23 -0400

    updated PM test, selenium test

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -4,15 +4,15 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from datetime import datetime
 import logging
 
-logging.basicConfig(filename="./seleniumlog.txt", format="%(asctime)s %(message)s",
+logging.basicConfig(filename="./seleniumlog630.log", format="%(asctime)s %(message)s",
                     filemode="w", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 # Start the browser and login with standard_user
-def login (user, password):
+def login(user, password):
     print('Starting the browser...')
     logging.info('Starting the browser...')
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=options)
     # driver = webdriver.Chrome()
@@ -20,7 +20,7 @@ def login (user, password):
     logging.info('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
     driver.maximize_window()
-    #login
+    # login
     print('Now logging in')
     logging.info('Now logging in ')
     driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
@@ -48,7 +48,6 @@ def login (user, password):
     else:
         print("Cart Adding Finished!")
         logging.info("Cart Adding Finished!")
-
 
     # Remove items from cart
     print("Removing all products from cart")
